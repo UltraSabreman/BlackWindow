93430a79d4cbce843e51339fe5d553fa38329cce
Andrey byelogurov <byelogurov@gmail.com>
2015-12-06 14:02:25 -0800

    Initial commit

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApplication1 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e) {
+            new MainWindow().Show();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+    }
+}
